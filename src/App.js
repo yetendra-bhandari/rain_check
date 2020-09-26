f6d4b6d61e7dd1af230f8502ac7cfaba26b1310d
Yetendra Bhandari <yetendrabhandari@gmail.com>
2020-09-26 16:32:38 +0530

    changed from BrowserRouterto HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navigation from "./Components/Navigation";
 import Home from "./Components/Home";
